5f59d992282ddf165e306a7ddc915c8e99685ceb
Mariia Fedirko (Nazarova) <fedirko.mary@gmail.com>
2016-02-01 15:06:13 +0200

    hover menu

--- JS/lesson_9,10/js/script.js
+++ JS/lesson_9,10/js/script.js
@@ -1,8 +1,24 @@
-$(function () {
-	var $links = $('.menu a');
-	$links.on('click', function(e) {
-		var $submenu = $(this).siblings('.submenu');
-		e.preventDefault();
-		$submenu.slideToggle();
-	});
-});
+// $(function () {
+// 	var $links = $('.menu a');
+// 	$links.on('click', function(e) {
+// 		var $submenu = $(this).siblings('.submenu');
+// 		e.preventDefault();
+// 		$submenu.slideToggle();
+// 	});
+// });
+
+$(function(){
+
+  var showDropdown = function () {
+    $(this).children('.submenu')
+      .show(300);
+  };
+
+  var hideDropdown = function () {
+    $(this).children('.submenu')
+      .hide(300);
+  };
+
+  $('li.sub').hover( showDropdown, hideDropdown );
+
+  });
