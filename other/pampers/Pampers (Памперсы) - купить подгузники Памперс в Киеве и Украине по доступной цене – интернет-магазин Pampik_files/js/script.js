35dc67dce79116899a718c125f0f02a597e680c1
Mariia Fedirko (Nazarova) <fedirko.mary@gmail.com>
2016-02-26 15:15:49 +0200

    new slider

--- other/pampers/Pampers (Памперсы) - купить подгузники Памперс в Киеве и Украине по доступной цене – интернет-магазин Pampik_files/js/script.js
+++ other/pampers/Pampers (Памперсы) - купить подгузники Памперс в Киеве и Украине по доступной цене – интернет-магазин Pampik_files/js/script.js
@@ -1,39 +1,7 @@
 $(function(){
-
-  $('.jcarousel').jcarousel({
-    wrap: 'circular',
-    transitions: true,
-      animation: {
-          fadeIn:400,
-          fadeOut:400,
-          // duration: 1200
-          easing:   'linear',
-          // complete: function() {
-          // }
-      }
-    })
-
-    .jcarouselAutoscroll({
-      interval: 6000,
-      target: '+=1',
-      autostart: true
-    });
-
-
-  $('.jcarousel_pagination')
-    .jcarouselPagination({
-        item: function(page) {
-            return '<a class = "jcarousel_page" href="#' + page + '"></a>';
-        }
-    })
-
-    .on('jcarouselpagination:active', 'a', function() {
-        $(this).addClass('active');
-    })
-
-    .on('jcarouselpagination:inactive', 'a', function() {
-        $(this).removeClass('active');
-    })
-  ;
-
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    auto: true
+  });
 });
