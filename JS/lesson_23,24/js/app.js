56f0b64afcafd04a83bb4b15b81573263a949f48
Mariia Fedirko (Nazarova) <fedirko.mary@gmail.com>
2016-03-23 14:59:19 +0200

    фЫВ

--- JS/lesson_23,24/js/app.js
+++ JS/lesson_23,24/js/app.js
@@ -1,24 +1,24 @@
-require.config({
-    paths: {
-        'jquery': 'http://code.jquery.com/jquery-1.12.0.min',
-    },
-    shim: {
-        'jquery': {
-            exports: 'jQuery'
-        }
-    }
-});
+// require.config({
+//     paths: {
+//         'jquery': 'http://code.jquery.com/jquery-1.12.0.min',
+//     },
+//     shim: {
+//         'jquery': {
+//             exports: 'jQuery'
+//         }
+//     }
+// });
 
 require(
     [
         'tmpl',
-        'jquery',
+        // 'jquery',
         'Model',
         'View',
         'Controller'
     ],
     
-    function (tmpl, $, Model, View, Controller) {
+    function (tmpl, Model, View, Controller) {
         $(function() {
 
             var data = ['Example'];
