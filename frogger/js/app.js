b7b63a33c7cccd963e46976f8e5651a261523b9b
nazmariam <fedirko.mary@gmail.com>
2018-12-31 23:30:51 +0200

    done

--- frogger/js/app.js
+++ frogger/js/app.js
@@ -6,27 +6,15 @@ const field = {
     w: 505,
     h: 606
 };
+// const randSpeed =
 let win = 0;
-
-// Enemies our player must avoid
 var Enemy = function(x,y,speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
     this.speed = speed;
 };
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x += dt*this.speed;
     if (checkCollision(this)){
         alert("You loose");
@@ -34,17 +22,12 @@ Enemy.prototype.update = function(dt) {
         player.y = start.y;
     }
 };
-
-// Draw the enemy on the screen, required method for game
-
 Enemy.prototype.render = function() {
     if(this.x >= field.w)
         this.x = -20;
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
 let Player = function(){
     this.sprite = 'images/char-boy.png';
     this.x = start.x;
@@ -89,7 +72,7 @@ Player.prototype.handleInput = function(code){
             if (this.y < 0) {
                 win++;
                 console.log(win);
-                alert("You win "+win+" times!");
+                alert("Happy New Year! You win "+win+" times!");
                 this.toStart();
             }
             break;
@@ -107,20 +90,14 @@ function checkCollision(enemy) {
         return false;
     }
 }
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
 let allEnemies = [
     new Enemy(-100, 55, 100),
     new Enemy(-400, 135, 70),
-    new Enemy(0, 220, 50)
+    new Enemy(0, 135, 35),
+    new Enemy(0, 220, 50),
+    new Enemy(-300, 220, 70)
 ];
 let player = new Player();
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -128,6 +105,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
