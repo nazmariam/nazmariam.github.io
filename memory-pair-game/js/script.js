2717aa3cceaa10de59d64eb0197bb831a544c526
nazmariam <fedirko.mary@gmail.com>
2019-01-02 21:43:05 +0200

    memory paired game done

--- memory-pair-game/js/script.js
+++ memory-pair-game/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function() {
     const cards = [
         "images/1.png",
         "images/2.png",
@@ -10,18 +10,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     cards.forEach(function (item) {
         cards.push(item);
     });
-
     let newArr = cards.sort(function() { return 0.5 - Math.random() });
-
     const main = document.querySelector('.main');
     const back = document.querySelectorAll('.back');
 
     for (let i=0; i<newArr.length; i++){
             back[i].innerHTML = '<img src=' + cards[i] + '>';
     }
-
     main.addEventListener('click', (e) => {
         let activeCards = document.querySelectorAll('.flip-container.active');
+        console.log(activeCards);
         let card = e.target;
         if(activeCards.length===2){
             activeCards.forEach(function (card) {
@@ -29,11 +27,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
             })
         }
         card.closest('.flip-container').classList.add('active');
-
+        activeCards = document.querySelectorAll('.flip-container.active');
+        if(activeCards.length===2){
+            let etalon = '', same = false;
+            activeCards.forEach(function (card) {
+                if (etalon === card.querySelector('img').src){
+                    same = true;
+                }
+                etalon = card.querySelector('img').src;
+                console.log(card.querySelector('img').src);
+            });
+            if (same){
+                console.log(same);
+                activeCards.forEach(function (card) {
+                    card.classList.add('hidden');
+                })
+            }
+        }
+        let hiddenCards = document.querySelectorAll('.hidden');
+        if(hiddenCards.length===cards.length){
+            let win = document.querySelector('h1');
+            win.classList.add('winner');
+        }
     });
-
-
-
-
-
 });
