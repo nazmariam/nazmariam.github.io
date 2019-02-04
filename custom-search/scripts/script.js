dbeb4dd713576cf72d8b88b236506459f0e72829
nazmariam <fedirko.mary@gmail.com>
2019-02-05 00:18:47 +0200

    search init

--- custom-search/scripts/script.js
+++ custom-search/scripts/script.js
@@ -1,9 +1,31 @@
-window.onload = () => {
-    function hndlr(response) {
-        for (var i = 0; i < response.items.length; i++) {
-            var item = response.items[i];
-            // in production code, item.htmlTitle should have the HTML entities escaped.
-            document.querySelector(".result").innerHTML += "<br>" + item.htmlTitle;
-        }
+document.querySelector("form").addEventListener("submit", function(event){
+    event.preventDefault();
+    let searchValue = event.target.elements[0].value;
+    search(searchValue);
+});
+let myData = [];
+function search(searchString){
+    searchString = searchString.toLowerCase();
+    let requestString = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyBS3H2dT37lO8RSEXjFkIC3g1cJNPdg8z4&cx=017576662512468239146:omuauf_lfve&q='+searchString;
+    fetch(requestString)
+        .then(function(response) {
+            response.json()
+                .then(data => {
+                    myData = data;
+                    showSearchResults(data);
+                });
+        }).catch(function (){
+        console.log("Oops! Looks like there is a problem");
+    });
+}
+function showSearchResults(response) {
+    console.log(response.items);
+    for (let i = 0; i < response.items.length; i++) {
+
+        let item = response.items[i];
+        console.log(item);
+        // in production code, item.htmlTitle should have the HTML entities escaped.
+        document.getElementById("content").innerHTML += "<br>" + item.htmlTitle;
     }
-};
+}
+
