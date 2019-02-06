376f1e95f6cc346ff0d79cf637be0df7feeb4ed1
nazmariam <fedirko.mary@gmail.com>
2019-02-06 11:41:36 +0200

    lucky button fixed

--- google_popup/dist/scripts/script.js
+++ google_popup/dist/scripts/script.js
@@ -2,26 +2,58 @@ document.querySelector("form").addEventListener("submit", function(event){
     event.preventDefault();
     let searchValue = event.target.elements[0].value;
     search(searchValue);
-    console.log();
 });
 document.getElementById('rt').addEventListener('click', function (event){
-	console.log('szxdgxcfgvhj');
+
 	let searchValue = document.getElementById('search').value;
-	console.log(searchValue);
-	search(searchValue);
-	// window.location.replace("https://www.google.com/doodles/");
+
+	if(searchValue){
+        searchFirst(searchValue);
+    }else{
+        window.location.replace("https://www.google.com/doodles/");
+    }
+
 });
 let myData=[];
 function search(searchString){
     searchString = searchString.toLowerCase();
-    let requestString = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyBS3H2dT37lO8RSEXjFkIC3g1cJNPdg8z4&cx=017576662512468239146:omuauf_lfve&q='+searchString;
+    let requestString = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyA6q0LrqOBAg0RyNnTf4p8lLiYVZ9LFdeU&cx=017576662512468239146:omuauf_lfve&q='+searchString;
     if(searchString){
 		fetch(requestString)
+            .then(function(response) {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response;
+            })
 			.then(function(response) {
 				response.json()
 					.then(data => {
 						myData = data;
-						showSearchResults(data);
+						showSearchResults(myData);
+					});
+			}).catch(function (){
+			console.log("Oops! Looks like there is a problem");
+		});
+	}
+}
+function searchFirst(searchString){
+    searchString = searchString.toLowerCase();
+    let requestString = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyA6q0LrqOBAg0RyNnTf4p8lLiYVZ9LFdeU&cx=017576662512468239146:omuauf_lfve&q='+searchString;
+    if(searchString){
+		fetch(requestString)
+            .then(function(response) {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response;
+            })
+			.then(function(response) {
+				response.json()
+					.then(data => {
+						myData = data;
+						showFirstResult(myData);
+						return myData;
 					});
 			}).catch(function (){
 			console.log("Oops! Looks like there is a problem");
@@ -40,5 +72,13 @@ function showSearchResults(response) {
 	result.innerHTML = '';
 	result.appendChild(ul);
 	document.querySelector('.search-form').classList.add('full-height');
+}
+function showFirstResult(response) {
+	let ul = document.createElement("ul");
+	let all = response.items;
+	if(all){
+        window.location.replace(all[1].link);
+	}
+
 }
 
